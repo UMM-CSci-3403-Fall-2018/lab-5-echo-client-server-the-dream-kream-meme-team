5260b1f5138ab347122f644a497ac040dbf44b0c
schon094 <schon094@morris.umn.edu>
2018-10-16 11:15:29 -0500

    started EchoServer and EchoClient.java (does not compile)

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,35 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
+  public static final int portNumber = 6013;
+
+  public static void main(String[] args) throws IOException {
+    String server;
+
+    if (args.length == 0) {
+      server = "127.0.0.1";
+    } else {
+      server = args[0];
+    }
+
+    try {
+      //inputStream.read, outputStream.write
+
+      //connect to sever
+      Socket socket = new Socket(server, portNumber);
+
+      
+    }
+
+    catch (ConnectException ce) {
+      System.out.println("We were unable to connect to " + server);
+      System.out.println("You should make sure the server is running.");
+    }
+    catch (IOException ioe) {
+      System.out.println("We caught an unexpected exception");
+      System.err.println(ioe);
+    }
+  }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,26 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoServer {
+  public static final int portNumber = 6013;
+
+  ServerSocket sock = new ServerSocket(portNumber);
+
+  while(true) {
+    Socket client = sock.accept();
+    System.out.println("Got a request!");
+
+    PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+
+    writer.println(new java.util.Date().toString());
+
+    client.close();
+
+  }
+
+  catch (IOException ioe) {
+    System.out.println("We caught an unexpected exception");
+    System.err.println(ioe);
+  }
 }

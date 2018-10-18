70258a723ee4466e910420a8f051b45883a83a72
unknown <lolgfman@gmail.com>
2018-10-18 02:42:42 -0500

    Changed the files to maybe handle input and output

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,4 @@
-package echoserver;
+
 import java.net.*;
 import java.io.*;
 
@@ -19,17 +19,21 @@ public class EchoClient {
 
       //connect to sever
       Socket socket = new Socket(server, portNumber);
-
-      
-    }
-
-    catch (ConnectException ce) {
+      //getting ready to read from the socket
+      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        //Recieving text from the Server
+            String line = in.readLine();
+            System.out.println(line);
+        }
+     catch (ConnectException ce) {
       System.out.println("We were unable to connect to " + server);
       System.out.println("You should make sure the server is running.");
-    }
-    catch (IOException ioe) {
+    } catch (IOException ioe) {
       System.out.println("We caught an unexpected exception");
       System.err.println(ioe);
     }
+
   }
+
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,8 +1,7 @@
-package echoserver;
 import java.net.*;
 import java.io.*;
 
-public class EchoServer {
+public class Server {
   public static final int portNumber = 6013;
 
 
@@ -10,20 +9,21 @@ public class EchoServer {
     try {
       ServerSocket sock = new ServerSocket(portNumber);
       while (true) {
-         Socket client= sock.accept();
+        Socket client = sock.accept();
         System.out.println("Got a request!");
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        PrintWriter out = new PrintWriter(client.getOutputStream(),true);
+        String line = in.readLine();
+        //Send data back to client
+        out.println(line);
 
-        PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
 
-        writer.println(new java.util.Date().toString());
-
-        client.close();
 
       }
     }
     catch(IOException ioe){
-    System.out.println("We caught an unexpected exception");
-    System.err.println(ioe);
+      System.out.println("We caught an unexpected exception");
+      System.err.println(ioe);
     }
   }
 }
